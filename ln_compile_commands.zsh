50012b31fd90e58e15329753b942222ae1a3a526
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2020-02-14 16:09:45 +0900

    refine ln

--- ln_compile_commands.zsh
+++ ln_compile_commands.zsh
@@ -6,17 +6,17 @@ source /opt/ros/$ROS_DISTRO/setup.zsh
 export COLCON_HOME=$PWD
 export COLCON_BUILD_PATH=$COLCON_HOME"/build"
 
-_c_paths=$(colcon list -p)
-echo $_c_paths | while read _c_path
+colcon_list=$(colcon list -p)
+echo $colcon_list | while read package_dir
 do
-  _cmakelists_path=$_c_path"/CMakeLists.txt"
-  if [[ -e $_cmakelists_path ]]; then
-    project_name=$(echo $(cat $_cmakelists_path | grep "^project") | sed -e "s/^.*(\(.*\)).*$/\1/")
-    if [ -n "$project_name" ]; then
-      echo $project_name
-      origin_ccj_path=$COLCON_BUILD_PATH"/"$project_name"/compile_commands.json"
-      if [[ -e $origin_ccj_path ]]; then
-        ln -sf $origin_ccj_path $_c_path"/"
+  package_cmake=$package_dir"/CMakeLists.txt"
+  if [[ -e $package_cmake ]]; then
+    package_name=$(echo $(cat $package_cmake | grep "^project") | sed -e "s/^.*(\(.*\)).*$/\1/")
+    if [ -n "$package_name" ]; then
+      echo $package_name
+      compile_commands=$COLCON_BUILD_PATH"/"$package_name"/compile_commands.json"
+      if [[ -e $compile_commands ]]; then
+        ln -sf $compile_commands $package_dir"/"
       fi
     fi
   fi
