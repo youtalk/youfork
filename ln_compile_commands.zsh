1a6ac326b39e95fd65d957f53158d8b0898edb51
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2020-02-14 16:06:36 +0900

    update ln

--- ln_compile_commands.zsh
+++ ln_compile_commands.zsh
@@ -1,28 +1,23 @@
 #!/bin/zsh
 (){
+cd `dirname $0`
+
 source /opt/ros/$ROS_DISTRO/setup.zsh
-export COLCON_HOME=$PWD"/"
-export COLCON_LOG_PATH=$COLCON_HOME"log/"
+export COLCON_HOME=$PWD
+export COLCON_BUILD_PATH=$COLCON_HOME"/build"
 
 _c_paths=$(colcon list -p)
-
 echo $_c_paths | while read _c_path
 do
   _cmakelists_path=$_c_path"/CMakeLists.txt"
   if [[ -e $_cmakelists_path ]]; then
     project_name=$(echo $(cat $_cmakelists_path | grep "^project") | sed -e "s/^.*(\(.*\)).*$/\1/")
     if [ -n "$project_name" ]; then
-      num=$(echo $_cmakelists_path | sed -e "s/^\///" | sed -e "s/\//\n/g" | wc -l)
-      for i in {$num..2}
-      do
-        cand_path=$(echo $_cmakelists_path | cut -d "/" -f1-$i)"/build"
-        if [[ -e $cand_path"/.built_by" ]]; then
-          origin_ccj_path=$cand_path"/"$project_name"/compile_commands.json"
-          if [[ -e $origin_ccj_path ]]; then
-            ln -s $origin_ccj_path $_c_path"/"
-          fi
-        fi
-      done
+      echo $project_name
+      origin_ccj_path=$COLCON_BUILD_PATH"/"$project_name"/compile_commands.json"
+      if [[ -e $origin_ccj_path ]]; then
+        ln -sf $origin_ccj_path $_c_path"/"
+      fi
     fi
   fi
 done
