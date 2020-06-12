343f9c832ed7593688ffde9d013458a48ba368fa
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2020-06-12 16:21:29 +0900

    add hpp and cpp

---
+++ src/youfork_teleop/include/youfork_teleop/youfork_teleop.hpp
@@ -0,0 +1,18 @@
+#ifndef YOUFORK_TELEOP_YOUFORK_TELEOP_HPP_
+#define YOUFORK_TELEOP_YOUFORK_TELEOP_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joy.hpp>
+#include <open_manipulator_msgs/srv/set_joint_position.hpp>
+
+namespace youfork_teleop
+{
+  class YouforkTeleop : public rclcpp::Node
+  {
+  public:
+    YouforkTeleop();
+  };
+}  // namespace youfork_teleop
+
+#endif  // YOUFORK_TELEOP_YOUFORK_TELEOP_HPP_
+
---
+++ src/youfork_teleop/src/youfork_teleop.cpp
@@ -0,0 +1,17 @@
+#include "youfork_teleop/youfork_teleop.hpp"
+
+namespace youfork_teleop
+{
+  YouforkTeleop::YouforkTeleop()
+    : Node("youfork_teleop")
+  {}
+}  // namespace youfork_teleop
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<youfork_teleop::YouforkTeleop>());
+  rclcpp::shutdown();
+
+  return 0;
+}
