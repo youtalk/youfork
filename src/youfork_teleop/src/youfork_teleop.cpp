d2599130c0f9dab573239ce450f255efe5fb5ae3
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2020-06-12 16:23:25 +0900

    fix lint

--- src/youfork_teleop/src/youfork_teleop.cpp
+++ src/youfork_teleop/src/youfork_teleop.cpp
@@ -2,12 +2,10 @@
 
 namespace youfork_teleop
 {
-  YouforkTeleop::YouforkTeleop()
-    : Node("youfork_teleop")
-  {}
+YouforkTeleop::YouforkTeleop() : Node("youfork_teleop") {}
 }  // namespace youfork_teleop
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<youfork_teleop::YouforkTeleop>());
